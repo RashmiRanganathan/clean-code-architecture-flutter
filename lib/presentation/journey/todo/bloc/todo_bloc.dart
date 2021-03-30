693f276e6b8e166137dc899b032baf5da19388bb
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 14:42:00 +0700

    add mapeventupdate

--- lib/presentation/journey/todo/bloc/todo_bloc.dart
+++ lib/presentation/journey/todo/bloc/todo_bloc.dart
@@ -4,14 +4,29 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_event.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_state.dart';
 
+import 'todo_event.dart';
+import 'todo_state.dart';
+
 class TodoBloc extends Bloc<TodoEvent, TodoState> {
   final Todousecase todousecase;
 
   TodoBloc({this.todousecase});
 
   @override
-  Stream<TodoState> mapEventToState(TodoEvent event) async* {}
+  Stream<TodoState> mapEventToState(TodoEvent event) async* {
+    if(event is UpdateTodo){
+      yield* updateMapEventToState(event.todoModel);
+    }
+  }
 
   @override
   TodoState get initialState => TodoInitial();
+
+  Stream<UpdateTodoSuccess> updateMapEventToState(event) async* {
+    final result = await todousecase.update(todoEntity: event);
+    if(result.isNotEmpty){
+      yield UpdateTodoSuccess();
+    }
+  }
+
 }
