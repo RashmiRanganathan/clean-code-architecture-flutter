3821e40e0a933eb54f762f0434c98cb927dc15b2
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 12:29:43 +0700

    add update screen constant

--- lib/common/constants/route_constants.dart
+++ lib/common/constants/route_constants.dart
@@ -2,4 +2,5 @@ class RouteList {
   static const String todoList = 'todo-list';
   static const String dashbaord = 'dashboard';
   static const String createTodo = 'create-todo';
+  static const String updateTodo = 'update-todo';
 }
