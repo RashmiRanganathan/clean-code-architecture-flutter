d1fdd9080858ccd07fd6edaa6284a58abe6193c0
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 12:34:10 +0700

    add update usecase

--- lib/domain/usescases/todo_usecase.dart
+++ lib/domain/usescases/todo_usecase.dart
@@ -1,7 +1,13 @@
 import 'package:clean_code_architecture_flutter/domain/repositories/todo_repository.dart';
 
+import '../entities/todo_entity.dart';
+
 class Todousecase {
   final TodoRespository todoRespository;
 
   Todousecase({this.todoRespository});
+
+  Future<String> update({TodoEntity todoModel}){
+    return todoRespository.update(todoModel);
+  }
 }
