05e16285698dd909fdc90211fc6c1f21acb3138d
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 12:26:32 +0700

    adding update repository

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -1,6 +1,7 @@
 import 'package:clean_code_architecture_flutter/data/models/todos_model.dart';
-
+import 'package:clean_code_architecture_flutter/domain/entities/todo_entity.dart';
 abstract class TodoRespository {
   Future<Todos> getTodos();
   Future<void> deleteTodoById({String id});
+  Future<String>update(TodoEntity todoModel);
 }
