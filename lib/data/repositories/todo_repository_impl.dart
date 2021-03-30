40d6f98bcbb5fed33cbe1c34607ddc0dc8351ffc
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 12:26:32 +0700

    adding update repository

--- lib/data/repositories/todo_repository_impl.dart
+++ lib/data/repositories/todo_repository_impl.dart
@@ -3,6 +3,8 @@ import 'package:clean_code_architecture_flutter/data/datasources/remote/todo_rem
 import 'package:clean_code_architecture_flutter/data/models/todos_model.dart';
 import 'package:clean_code_architecture_flutter/domain/repositories/todo_repository.dart';
 
+import '../../domain/entities/todo_entity.dart';
+
 class TodoRespositoryImpl extends TodoRespository {
   final TodoRemoteDatasource todoRemoteDatasource;
   final TodoLocalDatasource todoLocalDatasource;
@@ -14,13 +16,25 @@ class TodoRespositoryImpl extends TodoRespository {
 
   @override
   Future<Todos> getTodos() async => await todoRemoteDatasource.getTodos();
-  
+
   Future<void> deleteTodoById({String id}) async {
     try {
       await todoLocalDatasource.delete(id);
       await todoRemoteDatasource.deleteTodoById(id);
     } catch (e) {
       rethrow;
+    }
+  }
+
+  Future<String> update(TodoEntity todoModel) async {
+    try {
+      String id = todoModel.id;
+      Map<String, dynamic> payload = {'completed': todoModel.completed};
+      final String url = 'https://api-nodejs-todolist.herokuapp.com/task/$id';
+      final result = await todoRemoteDatasource.httpClient.put(url, payload);
+      return result;
+    } catch (e) {
+      throw Exception('Failed to update data');
     }
   }
 }
