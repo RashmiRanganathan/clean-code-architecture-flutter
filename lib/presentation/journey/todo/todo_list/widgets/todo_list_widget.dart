2f2b3e2abd0d3e454511931b663067de67fcccb4
Aldila Iskandar <aldila.iskndr@gmail.com>
2021-03-30 15:27:55 +0700

    implement slide to delete

--- lib/presentation/journey/todo/todo_list/widgets/todo_list_widget.dart
+++ lib/presentation/journey/todo/todo_list/widgets/todo_list_widget.dart
@@ -4,16 +4,37 @@ import 'package:flutter/material.dart';
 
 class TodoListWidget extends StatelessWidget {
   final TodoState state;
+  final Function(String id) onDismissable;
 
-  const TodoListWidget({Key key, this.state}) : super(key: key);
+  const TodoListWidget({Key key, this.state, this.onDismissable})
+      : super(key: key);
   @override
   Widget build(BuildContext context) {
     return ListView.builder(
       itemCount: state.todos.data.length,
       itemBuilder: (context, index) {
         final todos = state.todos.data[index];
-        return TodoCard(
-          text: '${todos.description}',
+        return Dismissible(
+          key: Key('card_todos_${todos.id}'),
+          direction: DismissDirection.endToStart,
+          background: Container(
+            color: Colors.red,
+            child: Align(
+              alignment: Alignment.centerRight,
+              child: Icon(
+                Icons.delete,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          onDismissed: (DismissDirection direction) {
+            if (direction == DismissDirection.endToStart) {
+              onDismissable?.call(todos.id);
+            }
+          },
+          child: TodoCard(
+            text: '${todos.description}',
+          ),
         );
       },
     );
