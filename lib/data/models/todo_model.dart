b91e04fcdeada9e73360a9914593695f02289a5f
Brian Esa Chrisnando <brian.esa@dkatalis.com>
2021-03-30 11:57:51 +0700

    add model and entity

--- lib/data/models/todo_model.dart
+++ lib/data/models/todo_model.dart
@@ -1,5 +1,34 @@
 import 'package:clean_code_architecture_flutter/domain/entities/todo_entity.dart';
-
 class TodoModel extends TodoEntity {
-  TodoModel() : super();
+  TodoModel({
+    String id,
+    String owner,
+    bool completed,
+    String description,
+    DateTime createdAt,
+    DateTime updatedAt
+  }) : super(
+     id: id,
+     owner: owner,
+     completed: completed,
+     description: description,
+     createdAt: createdAt,
+     updatedAt: updatedAt
+  );
+  factory TodoModel.fromJson(dynamic json){
+    return TodoModel(
+      id: json['_id'],
+      owner: json['owner'],
+      completed: json['completed'],
+      description: json['description'],
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt'])
+    );
+  }
+  Map<String,dynamic>  toJson(){
+    return {
+      'description' : description,
+      'completed': completed
+    };
+  }
 }
