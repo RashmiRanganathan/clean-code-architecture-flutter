edc25c48fa438db51f6d0385d0c4df0a2ebf2574
Jayesh K <jayesh.kapadnis@gmail.com>
2021-03-30 11:57:22 +0530

    Update entity to model conversion in bloc

--- lib/presentation/journey/todo/bloc/todo_bloc.dart
+++ lib/presentation/journey/todo/bloc/todo_bloc.dart
@@ -4,6 +4,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_event.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_state.dart';
 
+import '../../../../data/models/todo_model.dart';
 import 'todo_event.dart';
 import 'todo_state.dart';
 
@@ -25,7 +26,8 @@ class TodoBloc extends Bloc<TodoEvent, TodoState> {
   TodoState get initialState => TodoInitial();
 
   Stream<TodoState> _mapAddTodoEvent(AddTodoEvent event) async* {
-    await todousecase.createTodo(event.entity);
+    await todousecase
+        .createTodo(TodoModel(description: event.entity.description));
     yield CreateTodoSuccessfulState();
   }
 }
