5b3576337b4335a9b982eb7822f11646d34bb1f9
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 11:43:38 +0700

    add model to do

--- lib/domain/entities/todo_entity.dart
+++ lib/domain/entities/todo_entity.dart
@@ -1,3 +1,9 @@
 class TodoEntity {
-  TodoEntity();
+  String id;
+  String owner;
+  bool completed;
+  String description;
+  DateTime createdAt;
+  DateTime updatedAt;
+  TodoEntity({this.id, this.owner, this.completed, this.description, this.createdAt, this.updatedAt});
 }
