6c91a86bcaafaa8cb62cf106ea7d4248406deb86
Aldila Iskandar <aldila.iskndr@gmail.com>
2021-03-30 12:34:47 +0700

    create repository for delete todo function

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -2,4 +2,5 @@ import 'package:clean_code_architecture_flutter/data/models/todos_model.dart';
 
 abstract class TodoRespository {
   Future<Todos> getTodos();
+  Future<void> deleteTodoById({String id});
 }
