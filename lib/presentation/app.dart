84b853f6301cd6a5a122cd302e5a9d20054d2304
Aldila Iskandar <aldila.iskndr@gmail.com>
2021-03-30 11:47:28 +0700

    initial commit

--- lib/presentation/app.dart
+++ lib/presentation/app.dart
@@ -5,6 +5,7 @@ import 'package:clean_code_architecture_flutter/presentation/routes.dart';
 import 'package:clean_code_architecture_flutter/presentation/themes/theme_data.dart';
 
 class App extends StatelessWidget {
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
