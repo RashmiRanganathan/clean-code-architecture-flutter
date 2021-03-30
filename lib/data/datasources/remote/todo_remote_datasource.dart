81a7b46f2130f967dd9f3662e93acbc2bc467c92
Aldila Iskandar <aldila.iskndr@gmail.com>
2021-03-30 12:34:12 +0700

    create datasource delete todo function

--- lib/data/datasources/remote/todo_remote_datasource.dart
+++ lib/data/datasources/remote/todo_remote_datasource.dart
@@ -3,4 +3,10 @@ import 'package:clean_code_architecture_flutter/common/http/http_client.dart';
 class TodoRemoteDatasource {
   final HttpClient httpClient;
   TodoRemoteDatasource({this.httpClient});
+
+  String baseUrl = 'https://api-nodejs-todolist.herokuapp.com/task';
+
+  Future<void> deleteTodoById(String id) async {
+    await httpClient.delete('$baseUrl/id');
+  }
 }
