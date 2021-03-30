5e07d740ed95bf16199338695d0623e4466f278d
ahmadrifka <ahmadrifka@gmail.com>
2021-03-30 12:34:10 +0700

    add update usecase

--- lib/domain/usescases/todo_usecase.dart
+++ lib/domain/usescases/todo_usecase.dart
@@ -1,14 +1,20 @@
 import 'package:clean_code_architecture_flutter/data/models/todos_model.dart';
 import 'package:clean_code_architecture_flutter/domain/repositories/todo_repository.dart';
 
+import '../entities/todo_entity.dart';
+
 class Todousecase {
   final TodoRespository todoRespository;
 
   Todousecase({this.todoRespository});
 
   Future<Todos> getTodos() => todoRespository.getTodos();
-  
+
   Future<void> deleteTodoById({String id}) async {
     await todoRespository.deleteTodoById(id: id);
+  }
+
+  Future<String> update({TodoEntity todoModel}) {
+    return todoRespository.update(todoModel);
   }
 }
