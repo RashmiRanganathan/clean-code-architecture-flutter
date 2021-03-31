94bf2c4fb6b904fb23d85debca5d92494a6f2e06
ahmadrifka <ahmadrifka@gmail.com>
2021-03-31 13:13:07 +0700

    remove unused import

--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -1,5 +1,4 @@
 import 'package:clean_code_architecture_flutter/data/models/todos_model.dart';
-import 'package:clean_code_architecture_flutter/domain/entities/todo_entity.dart';
 
 abstract class TodoRespository {
   Future<Todos> getTodos();
