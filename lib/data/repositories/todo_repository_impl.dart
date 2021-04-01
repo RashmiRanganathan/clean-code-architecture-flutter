85762ef85370e241a7fea3a4743eac84748383ae
dedy <dedyindra351@gmail.com>
2021-04-01 13:22:37 +0700

    add todo repository impl

--- lib/data/repositories/todo_repository_impl.dart
+++ lib/data/repositories/todo_repository_impl.dart
@@ -1,5 +1,8 @@
+
+import 'package:clean_code_architecture_flutter/common/exceptions/server_exception.dart';
 import 'package:clean_code_architecture_flutter/data/datasources/local/local_database/todo_local_datasource.dart';
 import 'package:clean_code_architecture_flutter/data/datasources/remote/todo_remote_datasource.dart';
+import 'package:clean_code_architecture_flutter/data/models/todo_model.dart';
 import 'package:clean_code_architecture_flutter/domain/repositories/todo_repository.dart';
 
 class TodoRespositoryImpl extends TodoRespository {
@@ -10,4 +13,45 @@ class TodoRespositoryImpl extends TodoRespository {
     this.todoRemoteDatasource,
     this.todoLocalDatasource,
   });
+
+  @override
+  Future<List<TodoModel>> fetchTodo() async {
+    try {
+      List<TodoModel> todoList = [];
+      final response = await todoRemoteDatasource.fetchTodo();
+      for (var item in response) {
+        todoList.add(TodoModel.fromJson(item));
+      }
+      return todoList;
+    } catch (e) {
+      throw ServerException(message: e, code: '500');
+    }
+  }
+
+  @override
+  Future<void> createTodo({Map payload}) async {
+    try {
+      await todoRemoteDatasource.createTodo(payload);
+    } catch (e) {
+      throw ServerException(message: e, code: '500');
+    }
+  }
+
+  @override
+  Future<void> deleteTodo({String id}) async {
+    try {
+      await todoRemoteDatasource.deleteTodo(id);
+    } catch (e) {
+      throw ServerException(message: e, code: '500');
+    }
+  }
+
+  @override
+  Future<void> updateTodo({String id, Map payload}) async {
+    try {
+      await todoRemoteDatasource.updateTodo(id, payload);
+    } catch (e) {
+      throw ServerException(message: e, code: '500');
+    }
+  }
 }
--- lib/domain/repositories/todo_repository.dart
+++ lib/domain/repositories/todo_repository.dart
@@ -1,1 +1,9 @@
-abstract class TodoRespository {}
+import 'package:clean_code_architecture_flutter/data/models/todo_model.dart';
+
+abstract class TodoRespository {
+  Future<List<TodoModel>> fetchTodo();
+  Future<void> createTodo({Map payload});
+  Future<void> deleteTodo({String id});
+  Future<void> updateTodo({String id, Map payload});
+
+}
