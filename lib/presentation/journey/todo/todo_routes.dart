2302fe03ad27c884c7bdea9f00ce324835684f87
dedy <dedyindra351@gmail.com>
2021-04-01 13:20:15 +0700

    refactro todo routes

--- lib/presentation/journey/todo/todo_routes.dart
+++ lib/presentation/journey/todo/todo_routes.dart
@@ -1,14 +1,19 @@
+import 'package:clean_code_architecture_flutter/common/injector/injector.dart';
+import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_bloc.dart';
 import 'package:flutter/material.dart';
 
 import 'package:clean_code_architecture_flutter/common/constants/route_constants.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/create_todo/create_todo_screen.dart';
 import 'package:clean_code_architecture_flutter/presentation/journey/todo/todo_list/todo_list_screen.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class TodoRoutes {
   static Map<String, WidgetBuilder> getAll() {
     return {
-      RouteList.todoList: (context) => TodoListScreen(),
-      RouteList.createTodo: (context) => CreateTodoScreen(),
+      RouteList.todoList: (context) => BlocProvider<TodoBloc>(
+          create: (context) => Injector.resolve<TodoBloc>(),child: TodoListScreen(),),
+      RouteList.createTodo: (context) =>  BlocProvider<TodoBloc>(
+        create: (context) => Injector.resolve<TodoBloc>(),child: CreateTodoScreen(),),
     };
   }
 }
