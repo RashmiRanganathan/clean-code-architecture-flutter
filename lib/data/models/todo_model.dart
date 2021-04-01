050725ee08af1dee590f995ae3579f47aeaa1654
dedy <dedyindra351@gmail.com>
2021-04-01 13:21:59 +0700

    create entity model todo

--- lib/data/models/todo_model.dart
+++ lib/data/models/todo_model.dart
@@ -1,5 +1,38 @@
 import 'package:clean_code_architecture_flutter/domain/entities/todo_entity.dart';
-
 class TodoModel extends TodoEntity {
-  TodoModel() : super();
+  TodoModel({
+    String id,
+    String owner,
+    bool completed,
+    String description,
+    DateTime createdAt,
+    DateTime updatedAt
+  }) : super(
+     id: id,
+     owner: owner,
+     completed: completed,
+     description: description,
+     createdAt: createdAt,
+     updatedAt: updatedAt
+  );
+  factory TodoModel.fromJson(dynamic json){
+    return TodoModel(
+      id: json['_id'],
+      owner: json['owner'] ??'',
+      completed: json['completed'] ?? false,
+      description: json['description'] ?? '',
+      createdAt: DateTime.parse(json['createdAt']),
+      updatedAt: DateTime.parse(json['updatedAt'])
+    );
+  }
+  Map<String,dynamic>  toJson(){
+    return {
+      '_id':id,
+      'description' : description,
+      'completed': completed,
+      'owner': owner,
+      'createAt':createdAt,
+      'updateAt':updatedAt,
+    };
+  }
 }
--- lib/domain/entities/todo_entity.dart
+++ lib/domain/entities/todo_entity.dart
@@ -1,3 +1,16 @@
 class TodoEntity {
-  TodoEntity();
+  String id;
+  String owner;
+  bool completed;
+  String description;
+  DateTime createdAt;
+  DateTime updatedAt;
+
+  TodoEntity(
+      {this.id,
+      this.owner,
+      this.completed,
+      this.description,
+      this.createdAt,
+      this.updatedAt});
 }
