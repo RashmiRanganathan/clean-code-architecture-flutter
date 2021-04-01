d29c7b6b1634fd53bf6380eb58c02a0c55e06354
dedy <dedyindra351@gmail.com>
2021-04-01 13:19:38 +0700

    create form  todo

--- lib/presentation/journey/todo/create_todo/create_todo_screen.dart
+++ lib/presentation/journey/todo/create_todo/create_todo_screen.dart
@@ -1,4 +1,6 @@
+import 'package:clean_code_architecture_flutter/presentation/journey/todo/bloc/todo_bloc.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class CreateTodoScreen extends StatefulWidget {
   CreateTodoScreen({
@@ -10,13 +12,20 @@ class CreateTodoScreen extends StatefulWidget {
 }
 
 class _CreateTodoScreenState extends State<CreateTodoScreen> {
+  TextEditingController descriptionController;
+  TodoBloc _todoBloc;
+
+
   @override
   void initState() {
+    descriptionController = TextEditingController();
+    _todoBloc = BlocProvider.of<TodoBloc>(context);
     super.initState();
   }
 
   @override
   void dispose() {
+    _todoBloc.close();
     super.dispose();
   }
 
@@ -25,9 +34,40 @@ class _CreateTodoScreenState extends State<CreateTodoScreen> {
         appBar: AppBar(
           title: Text('Create Todo'),
           centerTitle: true,
+          leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: (){
+            Navigator.of(context).pop();
+          }),
         ),
-        body: Center(
-          child: Text('CODE !'),
+        body: BlocConsumer<TodoBloc,TodoState>(
+          bloc:_todoBloc,
+          listener: (context,state){
+            if(state is CreateTodoSuccess){
+              Navigator.of(context).pop();
+            }
+          },
+          builder: (context, state) {
+            return Center(
+              child: Column(
+                children: [
+                  Container(
+                      margin: EdgeInsets.all(20),
+                      child: TextFormField(
+                        controller: descriptionController,
+                        decoration: const InputDecoration(
+                          icon: Icon(Icons.description),
+                          labelText: 'description',
+                        ),
+                      )),
+                  RaisedButton(
+                      child: Text('create'),
+                      onPressed: () {
+                        print(descriptionController.text);
+                        _todoBloc.add(CreateTodo(description: descriptionController.text));
+                      })
+                ],
+              ),
+            );
+          }
         ),
       );
 }
